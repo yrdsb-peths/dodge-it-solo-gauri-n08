9c2c8214da0421e510d4e2ec72f5dec796ff22b2
gauri-n08 <gaurisknair08@gmail.com>
2025-04-15 09:05:00 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
-
+//This class 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 100, 100);
     }
 }
+
